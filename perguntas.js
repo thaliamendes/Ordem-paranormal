c55027fb2d5eb46b4f8b9e380f7c9a81d0edd7fd
thalia <163300504+thaliamendes@users.noreply.github.com>
2024-11-13 10:46:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que são os marcados',
+    'Os marcados são pessoas subostamen são escolhidas por entidades do outro lado para portarem um pouco do poder da entidade e estas entidades são divididas em elementos como sangem, ernegia, morte e conhecimento e tamnem tem mias um mas este elemnto não se mostra e nem se manifesta no mundo real e nem no mundo paranormal.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o pais onde a ordem foi criada?',
+    'A ordem foi criada em roma no seculo 18'
+)
+
+criaCartao(
+    'ordem paranormal',
+    'O que é a ordem ?',
+    'a ordem e um grupo de muitas pessoas trabalhando e lutando para manter o mundo sobrenatural em segrendo daqueles que acreditam no inposivel.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ordem em Inglês?',
+    'Ordem em ingles é order(order)'
+)
